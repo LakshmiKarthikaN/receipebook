60a18a785e1de136269a7b20113880fcbffcc800
LakshmiKarthikaN <132539671+LakshmiKarthikaN@users.noreply.github.com>
2025-04-06 19:10:49 +0530

    Add files via upload

---
+++ receipe.js
@@ -0,0 +1,71 @@
+const categoriesContainer = document.getElementById('categories');
+const mealsContainer = document.getElementById('meals');
+const searchBox = document.getElementById('searchBox');
+
+// Fetch and display all categories
+function loadCategories() {
+  fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
+    .then(res => res.json())
+    .then(data => {
+      categoriesContainer.innerHTML = '';
+      data.categories.forEach(category => {
+        const div = document.createElement('div');
+        div.className = 'card';
+        div.onclick = () => loadMealsByCategory(category.strCategory);
+        div.innerHTML = `
+          <img src="${category.strCategoryThumb}" alt="${category.strCategory}">
+          <h4>${category.strCategory}</h4>
+        `;
+        categoriesContainer.appendChild(div);
+      });
+    });
+}
+
+// Load meals by category
+function loadMealsByCategory(category) {
+  fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`)
+    .then(res => res.json())
+    .then(data => {
+      mealsContainer.innerHTML = '';
+      data.meals.forEach(meal => {
+        const div = document.createElement('div');
+        div.className = 'card';
+        div.innerHTML = `
+          <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
+          <h4>${meal.strMeal}</h4>
+        `;
+        mealsContainer.appendChild(div);
+      });
+    });
+}
+
+// Live search
+searchBox.addEventListener('input', function () {
+  const query = this.value.trim();
+  if (!query) {
+    mealsContainer.innerHTML = '';
+    return;
+  }
+
+  fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`)
+    .then(res => res.json())
+    .then(data => {
+      mealsContainer.innerHTML = '';
+      if (data.meals) {
+        data.meals.forEach(meal => {
+          const div = document.createElement('div');
+          div.className = 'card';
+          div.innerHTML = `
+            <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
+            <h4>${meal.strMeal}</h4>
+          `;
+          mealsContainer.appendChild(div);
+        });
+      } else {
+        mealsContainer.innerHTML = '<p>No recipes found.</p>';
+      }
+    });
+});
+
+// Initial load
+loadCategories();
